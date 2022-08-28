5bab29ee8efdd332be36b45a7be34c822dd71ea7
나성민 <77336125+Smuing@users.noreply.github.com>
2022-08-28 10:03:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 
 const app = express();
 
-const whitelist = ["https://smuing.github.io", "http://localhost:5500", "https://fo4hth.site"];
+const whitelist = ["https://smuing.github.io", "https://fo4hth.site", "http://localhost:5500", "http://127.0.0.1:5500"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
