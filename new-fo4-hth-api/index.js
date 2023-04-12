c24453de9af858dc6a22307667230e1f9c5b2472
Smuing <smuing0924@gmail.com>
2023-04-12 22:34:59 +0900

    get user info

--- new-fo4-hth-api/index.js
+++ new-fo4-hth-api/index.js
@@ -1,37 +1,63 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config();
+
+const API_KEY = process.env.API_KEY;
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 
-const originWhitelist = [FRONT_HOST];
+// const originWhitelist = [FRONT_HOST];
 
-app.use((req, res, next) => {
-  if (originWhitelist.indexOf(req.headers.origin) === -1) {
-    console.log(req);
-    console.log(req.headers.origin);
-    res
-      .status(403)
-      .json({ message: "You don't have permission to access this resource." });
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   if (originWhitelist.indexOf(req.headers.origin) === -1) {
+//     console.log(req);
+//     console.log(req.headers.origin);
+//     res
+//       .status(403)
+//       .json({ message: "You don't have permission to access this resource." });
+//   } else {
+//     next();
+//   }
+// });
 
-app.get("/matchids", (req, res) => {
-  const firstInput = req.query.first;
-  const secondInput = req.query.second;
+app.get("/matchids", async (req, res, next) => {
+  const firstInput = req.query.first_user;
+  const secondInput = req.query.second_user;
 
   if (firstInput === undefined || secondInput === undefined) {
     res.status(404).json({ message: "{first_user} or {second_user} is required" });
   } else {
     var accessIds = [];
     var originalNickname = [];
+    var matchIds = [];
+
+    try {
+      const firstUserInfo = await axios({
+        url: `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${firstInput}`,
+        method: "get",
+        headers: { Authorization: API_KEY },
+      });
+      accessIds.push(firstUserInfo.data.accessId);
+      originalNickname.push(firstUserInfo.data.nickname);
+      const secondUserInfo = await axios({
+        url: `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${secondInput}`,
+        method: "get",
+        headers: { Authorization: API_KEY },
+      });
+      accessIds.push(secondUserInfo.data.accessId);
+      originalNickname.push(secondUserInfo.data.nickname);
+    } catch (err) {
+      if (err?.response?.data?.message == "User could not found") {
+        return res.status(404).json({ message: "User could not found" });
+      }
+      next(err);
+    }
   }
 });
 
